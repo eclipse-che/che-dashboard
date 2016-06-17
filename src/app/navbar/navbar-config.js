bmhaskar/che

CHE-1095: add new UD/IDE navigation experience
Goals:
- revamp the left navbar
- remove the mobile responsiveness
- add ability to jump into recent workspace
- improve the branding/consistency between UD/IDE

Signed-off-by: Oleksii Kurinnyi <okurinnyi@codenvy.com>

dashboard/src/app/navbar/navbar-config.js
@@ -14,6 +14,11 @@ import {CheNavBarCtrl} from './navbar.controller';
 import {CheNavBar} from './navbar.directive';
 import {NavBarSelectedCtrl} from './navbar-selected.controller';
 import {NavBarSelected} from './navbar-selected.directive';
+import {NavbarRecentWorkspacesCtrl} from './recent-workspaces/recent-workspaces.controller';
+import {NavbarRecentWorkspaces} from './recent-workspaces/recent-workspaces.directive';
+
+import {NavbarDropdownMenuCtrl} from './navbar-dropdown-menu/navbar-dropdown-menu.controller';
+import {NavbarDropdownMenu} from './navbar-dropdown-menu/navbar-dropdown-menu.directive';
 
 export class NavbarConfig {
 
@@ -22,5 +27,11 @@ export class NavbarConfig {
     register.controller('NavBarSelectedCtrl', NavBarSelectedCtrl);
     register.directive('cheNavBar', CheNavBar);
     register.directive('navBarSelected', NavBarSelected);
+
+    register.controller('NavbarRecentWorkspacesCtrl', NavbarRecentWorkspacesCtrl);
+    register.directive('navbarRecentWorkspaces', NavbarRecentWorkspaces);
+
+    register.controller('NavbarDropdownMenuCtrl', NavbarDropdownMenuCtrl);
+    register.directive('navbarDropdownMenu', NavbarDropdownMenu);
   }
 }

dashboard/src/app/navbar/recent-workspaces/recent-workspaces.directive.js
@@ -11,26 +11,25 @@
 'use strict';
 
 /**
- * This class is handling the controller for the item in navbar allowing to redirect to the IDE
- * @author Florent Benoit
+ * @ngdoc directive
+ * @name navbar.directive:NavbarRecentWorkspaces
+ * @description This class is handling the directive of the listing recent opened workspaces in the navbar
+ * @author Oleksii Kurinnyi
  */
-class IdeListItemNavbar {
+export class NavbarRecentWorkspaces {
+
 
   /**
    * Default constructor that is using resource
    * @ngInject for Dependency injection
    */
-  constructor () {
-    this.restrict='E';
-    this.templateUrl = 'app/ide/ide-list-item-navbar/ide-list-item-navbar.html';
+  constructor() {
+    this.restrict = 'E';
+    this.templateUrl = 'app/navbar/recent-workspaces/recent-workspaces.html';
 
-
-    this.controller = 'IdeListItemNavbarCtrl';
-    this.controllerAs = 'ideListItemNavbarCtrl';
+    this.controller = 'NavbarRecentWorkspacesCtrl';
+    this.controllerAs = 'navbarRecentWorkspacesCtrl';
     this.bindToController = true;
   }
 
 }
-
-export default IdeListItemNavbar;
-

eclipse-che/che-dashboard

src/app/navbar/navbar-config.js
@@ -14,6 +14,11 @@ import {CheNavBarCtrl} from './navbar.controller';
 import {CheNavBar} from './navbar.directive';
 import {NavBarSelectedCtrl} from './navbar-selected.controller';
 import {NavBarSelected} from './navbar-selected.directive';
+import {NavbarRecentWorkspacesController} from './recent-workspaces/recent-workspaces.controller';
+import {NavbarRecentWorkspaces} from './recent-workspaces/recent-workspaces.directive';
+
+import {NavbarDropdownMenuController} from './navbar-dropdown-menu/navbar-dropdown-menu.controller';
+import {NavbarDropdownMenu} from './navbar-dropdown-menu/navbar-dropdown-menu.directive';
 
 export class NavbarConfig {
 
@@ -22,5 +27,11 @@ export class NavbarConfig {
     register.controller('NavBarSelectedCtrl', NavBarSelectedCtrl);
     register.directive('cheNavBar', CheNavBar);
     register.directive('navBarSelected', NavBarSelected);
+
+    register.controller('NavbarRecentWorkspacesController', NavbarRecentWorkspacesController);
+    register.directive('navbarRecentWorkspaces', NavbarRecentWorkspaces);
+
+    register.controller('NavbarDropdownMenuController', NavbarDropdownMenuController);
+    register.directive('navbarDropdownMenu', NavbarDropdownMenu);
   }
 }

src/app/navbar/recent-workspaces/recent-workspaces.directive.js
@@ -11,26 +11,25 @@
 'use strict';
 
 /**
- * Defines a directive for displaying full screen loader displayed to load the IDE
- * @author Florent Benoit
+ * @ngdoc directive
+ * @name navbar.directive:NavbarRecentWorkspaces
+ * @description This class is handling the directive of the listing recent opened workspaces in the navbar
+ * @author Oleksii Kurinnyi
  */
-class IdeLoader {
+export class NavbarRecentWorkspaces {
+
 
   /**
    * Default constructor that is using resource
    * @ngInject for Dependency injection
    */
-  constructor () {
-    this.restrict='E';
-    this.templateUrl = 'app/ide/ide-loader/ide-loader.html';
+  constructor() {
+    this.restrict = 'E';
+    this.templateUrl = 'app/navbar/recent-workspaces/recent-workspaces.html';
 
-
-    this.controller = 'IdeLoaderCtrl';
-    this.controllerAs = 'ideLoaderCtrl';
+    this.controller = 'NavbarRecentWorkspacesController';
+    this.controllerAs = 'navbarRecentWorkspacesController';
     this.bindToController = true;
   }
 
 }
-
-export default IdeLoader;
-