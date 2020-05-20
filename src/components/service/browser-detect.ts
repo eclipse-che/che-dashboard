f75e632e8fc1c55f1991bdcd143fcd70a9a14d31
Oleksii Kurinnyi <okurinny@redhat.com>
2020-05-20 15:10:27 +0300

    Fix browser detection (#41)
    Tested on
    
    - Chrome - no warning
    - Chromium - no warning
    - Firefox - no warning
    - Safari - warning's shown
    - Opera - warning's shown
    
    Signed-off-by: Oleksii Kurinnyi <okurinny@redhat.com>

--- src/components/service/browser-detect.ts
+++ src/components/service/browser-detect.ts
@@ -36,7 +36,7 @@ export class DetectSupportedBrowserService {
 
   get isChrome(): boolean {
     // @ts-ignore
-    return !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+    return !!window.chrome && (!!window.chrome.runtime || (navigator.userAgent.indexOf(' OPR/') === -1 && navigator.userAgent.indexOf(' Edge/')));
   }
 
   get isFirefox(): boolean {
