d1d826e4ba8e6e2f944bde302048a3f356d85dce
Oleksii Kurinnyi <okurinny@redhat.com>
2020-08-12 09:24:49 +0300

    fixed devfile editor bugs
    Signed-off-by: Oleksii Kurinnyi <okurinny@redhat.com>

--- src/monaco-languages-setup.ts
+++ src/monaco-languages-setup.ts
@@ -27,36 +27,79 @@ function createDocument(model) {
 
 function registerYAMLCompletion() {
   monaco.languages.registerCompletionItemProvider(LANGUAGE_ID, {
-    provideCompletionItems(model, position) {
+    provideCompletionItems(model, position, context) {
       const document = createDocument(model);
       return yamlService
         .doComplete(document, m2p.asPosition(position.lineNumber, position.column), true)
         .then(list => {
           const completionResult = p2m.asCompletionResult(list);
-          if (!completionResult || !completionResult.items) {
+          if (!completionResult || !completionResult.suggestions) {
             return completionResult;
           }
           // convert completionResult into suggestions
           const defaultInsertTextRules = monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet;
-          const suggestions = completionResult.items.map(item => {
-            return {
-              label: item.label,
-              kind: item.kind,
-              documentation: item.documentation,
-              insertText: item.insertText && item.insertText.value ? item.insertText.value : item.insertText,
-              insertTextRules: item.insertTextRules ? item.insertTextRules : defaultInsertTextRules
-            };
+          const suggestions = completionResult.suggestions.map(suggestion => {
+            return Object.assign(suggestion, {
+              insertTextRules: suggestion.insertTextRules ? suggestion.insertTextRules : defaultInsertTextRules,
+              sortText: createSortText(suggestion.insertText)
+            });
           });
-          return {suggestions};
+          return { suggestions };
         });
     },
 
-    resolveCompletionItem(item) {
+    resolveCompletionItem(model, position, item) {
       return yamlService
         .doResolve(m2p.asCompletionItem(item))
         .then(result => p2m.asCompletionItem(result));
     },
   });
+
+  const sortTextCache = {};
+  function createSortText(pluginId: string): string {
+    if (sortTextCache[pluginId]) {
+      return sortTextCache[pluginId];
+    }
+
+    const [publisher, name, version] = pluginId.split('/');
+    const semverRE = /^\d+?\.\d+?\.\d+?/;
+
+    let versionText: string;
+    if (semverRE.test(version)) {
+      let [major, minor, rest] = version.split('.');
+      // align version numbers length with leading zeros
+      major = addLeadingZeros(major);
+      minor = addLeadingZeros(minor);
+      const patch = rest.replace(/^(\d+)/, addLeadingZeros);
+
+      versionText = `${major}.${minor}.${patch}`;
+    } else {
+      // prefix version with '-' to move `latest`, `next` and other on the top of the list
+      let prefix = '-';
+
+      // sort common version names
+      if (version === 'latest') {
+        prefix += '0';
+      } else if (version === 'next') {
+        prefix += '1';
+      } else if (version === 'nightly') {
+        prefix += '2';
+      }
+
+      versionText = `${prefix}${version}`;
+    }
+
+    const sortText = `${publisher}/${name}/${versionText}`;
+    sortTextCache[pluginId] = sortText;
+    return sortText;
+  }
+
+  function addLeadingZeros(number: string): string {
+    while (number.length < 4) {
+      number = '0' + number;
+    }
+    return number;
+  }
 }
 
 function registerYAMLDocumentSymbols() {
