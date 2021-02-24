92975caf1ea6d49ade113b7d7477871cc79bd2a6
Oleksii Orel <oorel@redhat.com>
2021-02-24 16:09:43 +0200

    fixup! fix URIs for devfile icon and devfile self-link in single-host mode

--- src/services/registry/devfiles.ts
+++ src/services/registry/devfiles.ts
@@ -16,10 +16,12 @@ import axios from 'axios';
 const axiosInstance = axios.create();
 
 function createURL(url: string, baseUrl: string): URL {
+
   // todo Remove it after fixing all source links https://github.com/eclipse/che/issues/19140
   if (/^\/(\w+)/.test(url)) {
     return new URL(`.${url}`, baseUrl);
   }
+
   return new URL(url, baseUrl);
 }
 
