1bc654e3845df539b237c17623cbb2dd317593cd
Tereza Stastna <tstastna@redhat.com>
2021-05-14 15:38:21 +0200

    Fix capitalization

--- src/pages/WorkspacesList/index.tsx
+++ src/pages/WorkspacesList/index.tsx
@@ -143,7 +143,7 @@ export default class WorkspacesList extends React.PureComponent<Props, State> {
 
     return [
       {
-        title: 'Open in Verbose Mode',
+        title: 'Open in Verbose mode',
         isDisabled: false === this.isEnabledAction(WorkspaceAction.START_DEBUG_AND_OPEN_LOGS, workspace),
         onClick: (event, rowId, rowData) => this.handleAction(WorkspaceAction.START_DEBUG_AND_OPEN_LOGS, rowData),
 
