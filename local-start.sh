82bb1390886be1e2befb6ab95619e743de948830
Sergii Leshchenko <sleshche@redhat.com>
2021-09-10 13:50:02 +0300

    Make local run for development clearer

--- local-start.sh
+++ local-start.sh
@@ -1,32 +1,75 @@
 #!/bin/bash
 
-# Simple script that helps to build local version to test easily
 set -e
 set -u
 
-DASHBOARD_FRONTEND=packages/dashboard-frontend
-DASHBOARD_BACKEND=packages/dashboard-backend
+usage () {
+cat << EOF
+This scripts helps to build and run locally dashboard backend
+with frontend included against remote Che Cluster.
 
-# check if frontend is ready; run build otherwise
-if [ ! -d $DASHBOARD_FRONTEND/lib ] || [ -z "$(ls -A $DASHBOARD_FRONTEND/lib)" ]; then
-    yarn --cwd $DASHBOARD_FRONTEND build
-fi
+Prerequisite for usage is having a kubernetes cluster in KUBECONFIG
+and have access to Che namespace (use $CHE_NAMESPACE env var to configure it).
 
+Arguments:
+  -f|--force-build : by default packages are compiled only when they were not previously.
+                      This option should be used if compiled files must be overridden with fresher version.
+Env vars:
+  KUBECONFIG    : Kubeconfig file location. Default: "$HOME/.kube/config"
+  CHE_NAMESPACE : kubernetes namespace where Che Cluster should be looked into. Default: "eclipse-che"
+Examples:
+$0
+$0 --force-build
+EOF
+}
+
+parse_args() {
+  while [[ "$#" -gt 0 ]]; do
+    case $1 in
+      '-f'|'--force-build') FORCE_BUILD="true"; shift 0;;
+      '--help') usage; exit 0;;
+      *) echo "[ERROR] Unknown parameter is used: $1."; usage; exit 1;;
+    esac
+    shift 1
+  done
+}
+
+SCRIPT_DIR=$(dirname $(readlink -f "$0"))
+
+FORCE_BUILD="false"
 # Init Che Namespace with the default value if it's not set
 CHE_NAMESPACE="${CHE_NAMESPACE:-eclipse-che}"
 
 # guide backend to use the current cluster from kubeconfig
 export LOCAL_RUN="true"
-export KUBECONFIG=$HOME/.kube/config
+export KUBECONFIG="${KUBECONFIG:-$HOME/.kube/config}"
 
-# consider renaming it to CHE_API_URL since it's not just host
+SCRIPT_DIR=$(dirname $(readlink -f "$0"))
+DASHBOARD_FRONTEND=${SCRIPT_DIR}/packages/dashboard-frontend
+DASHBOARD_BACKEND=${SCRIPT_DIR}/packages/dashboard-backend
+
+parse_args "$@"
+
+if [ "$FORCE_BUILD" == "true" ] || \
+    [ ! -d $DASHBOARD_FRONTEND/lib ] || [ -z "$(ls -A $DASHBOARD_FRONTEND/lib)" ]; then
+  echo "[INFO] Compiling frontend"
+  yarn --cwd $DASHBOARD_FRONTEND build:dev
+fi
+
+if [ "$FORCE_BUILD" == "true" ] || \
+    [ ! -d $DASHBOARD_BACKEND/lib ] || [ -z "$(ls -A $DASHBOARD_BACKEND/lib)" ]; then
+  echo "[INFO] Compiling backend"
+  yarn --cwd $DASHBOARD_BACKEND build:dev
+fi
+
 export CHE_HOST=http://localhost:8080
-export CHE_API_PROXY_UPSTREAM=$(oc get checluster -n $CHE_NAMESPACE eclipse-che -o=json | jq -r '.status.cheURL')
-yarn --cwd $DASHBOARD_BACKEND build:dev
+CHE_URL=$(oc get checluster -n $CHE_NAMESPACE eclipse-che -o=json | jq -r '.status.cheURL')
 
 # we use relative to the static server path which is starting with '../../' to serv the dashboard-frontend
 # build output directory. In the case of docker build dashboard-frontend output directory will be copied
 # into the './public' directory (default value for static server)
 PUBLIC_FOLDER=../../../../$DASHBOARD_FRONTEND/lib
 
-yarn --cwd $DASHBOARD_BACKEND start:debug --publicFolder $PUBLIC_FOLDER
+yarn --cwd $DASHBOARD_BACKEND start:debug \
+  --publicFolder $PUBLIC_FOLDER \
+  --cheApiUpstream $CHE_URL
