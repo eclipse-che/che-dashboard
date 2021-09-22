95791b2c8a17ef59190175abc3c090a08dc918aa
Oleksii Kurinnyi <okurinny@redhat.com>
2021-09-22 17:48:01 +0300

    chore: create eslint configuration and commands in backend

---
+++ packages/dashboard-backend/.eslintrc.js
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2018-2021 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+
+module.exports = {
+  env: {
+    node: true,
+  },
+  extends: [
+    '../../.eslintrc.js',
+  ],
+  rules: {
+    'notice/notice': [
+      'error',
+      {
+        templateFile: '../../.config/copyright.js',
+        onNonMatchingHeader: 'report',
+        messages: {
+          reportAndSkip: 'Missing license header',
+        },
+      },
+    ],
+  },
+};
