b5c51f08b559af067b516ca1e949cef375b781d1
Oleksii Kurinnyi <okurinny@redhat.com>
2021-10-25 13:13:11 +0300

    Dashboard reports its version (#380)
    Co-authored-by: Nick Boldt <nboldt@redhat.com>

--- packages/dashboard-frontend/src/Layout/Header/Tools/about-modal.tsx
+++ packages/dashboard-frontend/src/Layout/Header/Tools/about-modal.tsx
@@ -21,7 +21,7 @@ import { detect } from 'detect-browser';
 
 type AboutModalProps = {
   productName: string | undefined;
-  productVersion: string | undefined;
+  serverVersion: string | undefined;
   logo: string;
   isOpen: boolean;
   closeAboutModal: () => void;
@@ -30,7 +30,7 @@ type AboutModalProps = {
 
 type AboutModalItemsProps = {
   username: string | undefined;
-  productVersion: string | undefined;
+  serverVersion: string | undefined;
   browserVersion: string | null | undefined;
   browserOS: string | null | undefined;
   browserName: string | null | undefined;
@@ -39,7 +39,8 @@ type AboutModalItemsProps = {
 const AboutModalItems: React.FC<AboutModalItemsProps> = (
   props: AboutModalItemsProps
 ) => {
-  const productVersion = props.productVersion;
+  const dashboardVersion = process.env.DASHBOARD_VERSION;
+  const serverVersion = props.serverVersion;
   const username = props.username;
   const browserVersion = props.browserVersion;
   const browserOS = props.browserOS;
@@ -47,43 +48,75 @@ const AboutModalItems: React.FC<AboutModalItemsProps> = (
   return (
     <>
       <TextContent>
-        <TextList component='dl'>
-          {productVersion && (
+        <TextList component="dl">
+          {dashboardVersion && (
             <>
-              <TextListItem component='dt'>Version</TextListItem>
-              <TextListItem component='dd'>
-                <div className='co-select-to-copy'>{productVersion}</div>
+              <TextListItem component="dt">Dashboard Version</TextListItem>
+              <TextListItem
+                component="dd"
+                className="co-select-to-copy"
+                data-testid="dashboard-version"
+              >
+                {dashboardVersion}
+              </TextListItem>
+            </>
+          )}
+          {serverVersion && (
+            <>
+              <TextListItem component="dt">Server Version</TextListItem>
+              <TextListItem
+                component="dd"
+                className="co-select-to-copy"
+                data-testid="server-version"
+              >
+                {serverVersion}
               </TextListItem>
             </>
           )}
           {username && (
             <>
-              <TextListItem component='dt'>Username</TextListItem>
-              <TextListItem component='dd' className='co-select-to-copy'>
+              <TextListItem component="dt">Username</TextListItem>
+              <TextListItem
+                component="dd"
+                className="co-select-to-copy"
+                data-testid="username"
+              >
                 {username}
               </TextListItem>
             </>
           )}
           {browserName && (
             <>
-              <TextListItem component='dt'>Browser Name</TextListItem>
-              <TextListItem component='dd' className='co-select-to-copy'>
+              <TextListItem component="dt">Browser Name</TextListItem>
+              <TextListItem
+                component="dd"
+                className="co-select-to-copy"
+                data-testid="browser-name"
+              >
                 {browserName}
               </TextListItem>
             </>
           )}
           {browserVersion && (
             <>
-              <TextListItem component='dt'>Browser Version</TextListItem>
-              <TextListItem component='dd' className='co-select-to-copy'>
+              <TextListItem component="dt">Browser Version</TextListItem>
+              <TextListItem
+                component="dd"
+                className="co-select-to-copy"
+                data-testid="browser-version"
+              >
                 {browserVersion}
               </TextListItem>
             </>
           )}
           {browserOS && (
             <>
-              <TextListItem component='dt'>Browser OS</TextListItem>
-              <TextListItem component='dd' className='co-select-to-copy'>
+              <TextListItem component="dt">Browser OS</TextListItem>
+              <TextListItem
+                component="dd"
+                className="co-select-to-copy"
+                data-testid="browser-os"
+              >
                 {browserOS}
               </TextListItem>
             </>
@@ -99,7 +132,7 @@ export class AboutModal extends React.PureComponent<AboutModalProps> {
     const { isOpen, closeAboutModal } = this.props;
     const productName = this.props.productName;
     const logo = this.props.logo;
-    const productVersion = this.props.productVersion;
+    const serverVersion = this.props.serverVersion;
     const userName = this.props.username;
 
     const browser = detect();
@@ -116,7 +149,7 @@ export class AboutModal extends React.PureComponent<AboutModalProps> {
         noAboutModalBoxContentContainer={true}
       >
         <AboutModalItems
-          productVersion={productVersion}
+          serverVersion={serverVersion}
           username={userName}
           browserOS={browserOS}
           browserVersion={browserVersion}
