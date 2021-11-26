0ae0242c357ba8bf3e191e79ee0616377f4294c3
Oleksii Kurinnyi <okurinny@redhat.com>
2021-11-26 12:26:26 +0200

    fix: factory resolver to respect the OAuth response

--- packages/dashboard-frontend/src/store/FactoryResolver/index.ts
+++ packages/dashboard-frontend/src/store/FactoryResolver/index.ts
@@ -170,13 +170,10 @@ export const actionCreators: ActionCreators = {
         });
         return;
       } catch (e) {
-        if (
-          common.helpers.errors.isAxiosError(e) &&
-          common.helpers.errors.isAxiosResponse(e.response)
-        ) {
-          const responseData = e.response.data;
-          if (e.response.status === 401 && isOAuthResponse(responseData)) {
-            throw responseData;
+        if (common.helpers.errors.isAxiosResponse((e as any).response)) {
+          const response = (e as any).response;
+          if (response.status === 401 && isOAuthResponse(response.data)) {
+            throw response.data;
           }
         }
         const errorMessage =
