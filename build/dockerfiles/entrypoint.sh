ba0955ab2ee8dc51e23ef344d4a83a4fbed53ae4
dymart <dylanirur@outlook.com>
2022-08-01 18:13:54 -0700

    [build] feat: add env to set endpoints for non standard oidc auth in kubernetes

--- build/dockerfiles/entrypoint.sh
+++ build/dockerfiles/entrypoint.sh
@@ -8,6 +8,30 @@
 # SPDX-License-Identifier: EPL-2.0
 #
 
+set -a
+
+if [ ! -z "$CHE_INFRA_KUBERNETES_PORT" ]
+then
+    KUBERNETES_PORT=$CHE_INFRA_KUBERNETES_PORT
+fi
+
+if [ ! -z "$CHE_INFRA_KUBERNETES_PORT_443_TCP_ADDR" ]
+then
+    KUBERNETES_PORT_443_TCP_ADDR=$CHE_INFRA_KUBERNETES_PORT_443_TCP_ADDR
+fi
+
+if [ ! -z "$CHE_INFRA_KUBERNETES_PORT_443_TCP" ]
+then
+    KUBERNETES_PORT_443_TCP=$CHE_INFRA_KUBERNETES_PORT_443_TCP
+fi
+
+if [ ! -z "$CHE_INFRA_KUBERNETES_SERVICE_HOST" ]
+then
+    KUBERNETES_SERVICE_HOST=$CHE_INFRA_KUBERNETES_SERVICE_HOST
+fi
+
+set +a
+
 set -e
 echo 'Starting Dashboard backend server...'
 start_server="node /backend/server/backend.js --publicFolder /public"
