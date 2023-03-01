6997cbf60252b6a8c4b5b4387f6a2450f749efc1
Valerii Svydenko <vsvydenk@redhat.com>
2023-03-01 10:55:41 +0200

    chore: refresh access token when workspace is starting
    Signed-off-by: Valerii Svydenko <vsvydenk@redhat.com>

--- packages/dashboard-frontend/src/services/oauth/index.ts
+++ packages/dashboard-frontend/src/services/oauth/index.ts
@@ -10,12 +10,12 @@
  *   Red Hat, Inc. - initial API and implementation
  */
 
-import { helpers } from '@eclipse-che/common';
+import common, { helpers } from '@eclipse-che/common';
 import { OAuthResponse } from '../../store/FactoryResolver';
 import SessionStorageService, { SessionStorageKey } from '../session-storage';
 import { container } from '../../inversify.config';
 import { CheWorkspaceClient } from '../workspace-client/cheworkspace/cheWorkspaceClient';
-import { Workspace } from '../workspace-adapter';
+import devfileApi from '../devfileApi';
 
 const WorkspaceClient = container.get(CheWorkspaceClient);
 
@@ -30,15 +30,37 @@ export default class OAuthService {
     }
   }
 
-  static async refreshTokenIfNeeded(workspace: Workspace): Promise<void> {
-    if (
-      SessionStorageService.get(SessionStorageKey.AUTHENTICATION_STATUS) !== 'started' &&
-      workspace.devfile.projects
-    ) {
-      SessionStorageService.update(SessionStorageKey.AUTHENTICATION_STATUS, 'started');
-      const project = workspace.devfile.projects.find(p => (p.name = workspace.projects[0]));
-      if (project && project.git) {
-        await WorkspaceClient.restApiClient.refreshFactoryOauthToken(project.git.remotes.origin);
+  static async refreshTokenIfNeeded(workspace: devfileApi.DevWorkspace): Promise<void> {
+    if (!workspace.spec.template.projects) {
+      return;
+    }
+
+    const project = workspace.spec.template.projects[0];
+    if (!project || !project.git) {
+      return;
+    }
+
+    try {
+      await WorkspaceClient.restApiClient.refreshFactoryOauthToken(project.git.remotes.origin);
+    } catch (e) {
+      if (!common.helpers.errors.includesAxiosResponse(e)) {
+        return;
+      }
+      const response = e.response;
+      if (response.status === 401 && isOAuthResponse(response.data)) {
+        // build redirect URL
+        const redirectUrl = new URL(
+          'dashboard/w',
+          window.location.protocol + '//' + window.location.host,
+        );
+        redirectUrl.searchParams.set(
+          'params',
+          `{"namespace":"${workspace.metadata.namespace}","workspace":"${workspace.metadata.name}"}`,
+        );
+        OAuthService.openOAuthPage(
+          response.data.attributes.oauth_authentication_url,
+          redirectUrl.toString(),
+        );
       }
     }
   }
