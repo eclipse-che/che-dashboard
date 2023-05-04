df59603b681eac640d80b9499bc487cefc42b559
Oleksii Orel <oorel@redhat.com>
2023-05-04 14:19:41 +0300

    fixup! fixup! fixup! fixup! fix:'404 page not found' error when open a workspace

--- packages/dashboard-frontend/src/services/helpers/api-ping.ts
+++ packages/dashboard-frontend/src/services/helpers/api-ping.ts
@@ -21,27 +21,35 @@ export async function isAvailableEndpoint(url: string | undefined): Promise<bool
   if (!url) {
     return false;
   }
+
   let attempt = 0;
+  let result = false;
   while (attempt < MAX_ATTEMPT_QUANTITY) {
     attempt++;
     try {
       await axios.get(url);
-      return true;
+      result = true;
+      break;
     } catch (error) {
-      if (
-        common.helpers.errors.includesAxiosResponse(error) &&
-        (error.response?.status === 404 || error.response?.status === 503)
-      ) {
-        if (attempt === MAX_ATTEMPT_QUANTITY) {
-          console.error(`Endpoint '${url}' is not available. ${helpers.errors.getMessage(error)}`);
-          return false;
-        }
-        await delay(DELAY_TIME);
-      } else {
-        return true;
+      const is404 =
+        common.helpers.errors.includesAxiosResponse(error) && error.response.status === 404;
+      const is503 =
+        common.helpers.errors.includesAxiosResponse(error) && error.response.status === 503;
+
+      if (is404 === false && is503 === false) {
+        result = true;
+        break;
       }
+
+      if (attempt === MAX_ATTEMPT_QUANTITY) {
+        console.error(`Endpoint '${url}' is not available. ${helpers.errors.getMessage(error)}`);
+        result = false;
+        break;
+      }
+
+      await delay(DELAY_TIME);
     }
   }
 
-  return false;
+  return result;
 }
