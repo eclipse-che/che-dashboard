16f3e047f63074ae49b5db039335e056b891b75f
Oleksii Orel <oorel@redhat.com>
2023-06-21 15:45:19 +0300

    fix: added an ability to delete devworkspaseTemplates for local development mode
    Signed-off-by: Oleksii Orel <oorel@redhat.com>

--- packages/dashboard-backend/src/routes/api/devworkspaceTemplates.ts
+++ packages/dashboard-backend/src/routes/api/devworkspaceTemplates.ts
@@ -10,18 +10,20 @@
  *   Red Hat, Inc. - initial API and implementation
  */
 
-import { FastifyInstance, FastifyRequest } from 'fastify';
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 import { baseApiPath } from '../../constants/config';
 import {
   namespacedSchema,
   namespacedTemplateSchema,
   dwTemplatePatchSchema,
   templateStartedSchema,
+  namespacedWorkspaceSchema,
 } from '../../constants/schemas';
 import { getDevWorkspaceClient } from './helpers/getDevWorkspaceClient';
 import { getToken } from './helpers/getToken';
 import { getSchema } from '../../services/helpers';
 import { restParams } from '../../models';
+import { isLocalRun } from '../../localRun';
 
 const tags = ['Devworkspace Template'];
 
@@ -71,4 +73,28 @@ export function registerDevWorkspaceTemplates(server: FastifyInstance) {
       return templateApi.patch(namespace, templateName, patch);
     },
   );
+
+  if (isLocalRun()) {
+    server.delete(
+      `${baseApiPath}/namespace/:namespace/devworkspacetemplates/:templateName`,
+      getSchema({
+        tags,
+        params: namespacedTemplateSchema,
+        response: {
+          204: {
+            description: 'The DevWorkspaceTemplate successfully deleted',
+            type: 'null',
+          },
+        },
+      }),
+      async function (request: FastifyRequest, reply: FastifyReply) {
+        const { namespace, templateName } = request.params as restParams.INamespacedTemplateParams;
+        const token = getToken(request);
+        const { devWorkspaceTemplateApi: templateApi } = getDevWorkspaceClient(token);
+        await templateApi.delete(namespace, templateName);
+        reply.code(204);
+        return reply.send();
+      },
+    );
+  }
 }
