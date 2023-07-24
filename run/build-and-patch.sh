7b1dfb110e9505463b1c52f726893dad0bc0997a
Oleksii Orel <oorel@redhat.com>
2023-07-24 19:43:55 +0300

    fix: Code refactoring
    Signed-off-by: Oleksii Orel <oorel@redhat.com>

--- run/build-and-patch.sh
+++ run/build-and-patch.sh
@@ -17,19 +17,11 @@ CHE_DASHBOARD_IMAGE="${IMAGE_REGISTRY_HOST}/${IMAGE_REGISTRY_USER_NAME}/che-dash
 
 echo "[INFO] Build a new image '${CHE_DASHBOARD_IMAGE}'..."
 
-if [[ -z "$(ps aux | grep -v grep | grep docker)" ]]; then
-  echo "[INFO] Using Podman platform."
-  TARGET_COMMAND='podman'
-else
-  echo "[INFO] Using Docker platform."
-  TARGET_COMMAND='docker'
-fi
-
-$TARGET_COMMAND build . -f build/dockerfiles/Dockerfile -t $CHE_DASHBOARD_IMAGE
+"${PWD}/scripts/container_tool.sh" build . -f build/dockerfiles/Dockerfile -t $CHE_DASHBOARD_IMAGE
 
 echo "[INFO] Push the image '${CHE_DASHBOARD_IMAGE}'..."
 
-$TARGET_COMMAND push $CHE_DASHBOARD_IMAGE
+"${PWD}/scripts/container_tool.sh" push $CHE_DASHBOARD_IMAGE
 
 echo "[INFO] Patching checluster with the new dashboard image '${CHE_DASHBOARD_IMAGE}'..."
 
