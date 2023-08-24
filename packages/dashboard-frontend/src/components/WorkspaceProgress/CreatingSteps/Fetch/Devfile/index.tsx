c0a44a1aedbe1cf9ec3af592818769833109399f
Oleksii Kurinnyi <okurinny@redhat.com>
2023-08-24 19:25:46 +0300

    fix: redirect to workspace creation page after authentication
    Signed-off-by: Oleksii Kurinnyi <okurinny@redhat.com>

--- packages/dashboard-frontend/src/components/WorkspaceProgress/CreatingSteps/Fetch/Devfile/index.tsx
+++ packages/dashboard-frontend/src/components/WorkspaceProgress/CreatingSteps/Fetch/Devfile/index.tsx
@@ -10,7 +10,7 @@
  *   Red Hat, Inc. - initial API and implementation
  */
 
-import common, { helpers } from '@eclipse-che/common';
+import common, { FACTORY_LINK_ATTR, helpers } from '@eclipse-che/common';
 import { AlertVariant } from '@patternfly/react-core';
 import { isEqual } from 'lodash';
 import React from 'react';
@@ -32,10 +32,10 @@ import {
 } from '../../../../../store/FactoryResolver/selectors';
 import { selectAllWorkspaces } from '../../../../../store/Workspaces/selectors';
 import ExpandableWarning from '../../../../ExpandableWarning';
-import { TIMEOUT_TO_RESOLVE_SEC } from '../../../const';
 import { ProgressStep, ProgressStepProps, ProgressStepState } from '../../../ProgressStep';
 import { ProgressStepTitle } from '../../../StepTitle';
 import { TimeLimit } from '../../../TimeLimit';
+import { TIMEOUT_TO_RESOLVE_SEC } from '../../../const';
 import { buildStepName } from './buildStepName';
 
 export class ApplyingDevfileError extends Error {
@@ -250,13 +250,19 @@ class CreatingStepFetchDevfile extends ProgressStep<Props, State> {
         // open authentication page
         const env = getEnvironment();
         // build redirect URL
-        let redirectHost = window.location.protocol + '//' + window.location.host;
+        let redirectHost = window.location.origin;
         if (isDevEnvironment(env)) {
-          redirectHost = env.server;
+          redirectHost = env.server || redirectHost;
         }
+
         const redirectUrl = new URL('/f', redirectHost);
-        redirectUrl.searchParams.set('url', factoryUrl);
+        redirectUrl.searchParams.set(
+          FACTORY_LINK_ATTR,
+          this.props.history.location.search.replace(/^\?/, ''),
+        );
+
         OAuthService.openOAuthPage(e.attributes.oauth_authentication_url, redirectUrl.toString());
+
         return false;
       }
 
