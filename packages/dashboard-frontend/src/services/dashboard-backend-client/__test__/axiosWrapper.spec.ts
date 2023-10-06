4c394cc4fea10baa07afd32fc87becb35c3a5c73
Anatolii Bazko <abazko@redhat.com>
2023-10-06 11:24:43 +0200

    fixup
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

--- packages/dashboard-frontend/src/services/dashboard-backend-client/__test__/axiosWrapper.spec.ts
+++ packages/dashboard-frontend/src/services/dashboard-backend-client/__test__/axiosWrapper.spec.ts
@@ -10,7 +10,7 @@
  *   Red Hat, Inc. - initial API and implementation
  */
 
-import axios, { AxiosInstance } from 'axios';
+import { AxiosInstance } from 'axios';
 import { AxiosWrapper } from '../axiosWrapper';
 import mockAxios from 'axios';
 
