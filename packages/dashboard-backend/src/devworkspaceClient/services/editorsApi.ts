9924679d457c22c2f163aa0dad162a1c1877ba1d
Valeriy Svydenko <vsvydenk@redhat.com>
2024-05-28 15:54:13 +0300

    add tests for editorsApi
    Signed-off-by: Valeriy Svydenko <vsvydenk@redhat.com>

--- packages/dashboard-backend/src/devworkspaceClient/services/editorsApi.ts
+++ packages/dashboard-backend/src/devworkspaceClient/services/editorsApi.ts
@@ -73,7 +73,7 @@ export class EditorsApiService implements IEditorsApi {
           const editor = yaml.load(cm.data[key]) as V222Devfile;
           editors.push(editor);
         } catch (error) {
-          logger.error(error, 'Failed to parse editor: %s', cm.data[key]);
+          logger.error(error, 'Failed to parse editor: %s from %s Config Map', key, cm.metadata?.name);
         }
       }
     }
