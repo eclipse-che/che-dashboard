672f90988523d54dfc7d9abed176cc8c07997cd4
Anatolii Bazko <abazko@redhat.com>
2024-06-27 09:25:19 +0200

    Fixup
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

--- packages/dashboard-frontend/src/contexts/WorkspaceActions/BulkDeleteButton/__mocks__/index.tsx
+++ packages/dashboard-frontend/src/contexts/WorkspaceActions/BulkDeleteButton/__mocks__/index.tsx
@@ -21,9 +21,7 @@ export class WorkspaceActionsBulkDeleteButton extends React.Component<Props> {
     const { isDisabled, onAction, workspaces } = this.props;
 
     const handleAction = () =>
-      workspaces.map(
-        workspace => onAction?.(WorkspaceAction.DELETE_WORKSPACE, workspace.uid, true),
-      );
+      workspaces.map(workspace => onAction?.(WorkspaceAction.DELETE_WORKSPACE, workspace.uid, true));
 
     return (
       <div data-testid="workspace-actions-bulk-delete">
