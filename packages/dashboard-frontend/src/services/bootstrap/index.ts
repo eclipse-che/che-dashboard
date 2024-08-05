45cab91fac150aa74b8b35d194e245907a2f65e3
Anatolii Bazko <abazko@redhat.com>
2024-08-05 09:46:10 +0200

    Server airgap samples
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

--- packages/dashboard-frontend/src/services/bootstrap/index.ts
+++ packages/dashboard-frontend/src/services/bootstrap/index.ts
@@ -331,22 +331,24 @@ export default class Bootstrap {
       undefined,
     );
 
-    const airGapedSampleURL = new URL('/dashboard/api/airgap-sample', window.location.origin).href;
-    await requestRegistriesMetadata(airGapedSampleURL, false)(
-      this.store.dispatch,
-      this.store.getState,
-      undefined,
-    );
-
     const serverConfig = this.store.getState().dwServerConfig.config;
     const devfileRegistry = serverConfig.devfileRegistry;
     const internalDevfileRegistryUrl = serverConfig.devfileRegistryURL;
     if (
       devfileRegistry?.disableInternalRegistry !== undefined &&
-      devfileRegistry?.disableInternalRegistry !== true &&
-      internalDevfileRegistryUrl
+      devfileRegistry?.disableInternalRegistry !== true
     ) {
-      await requestRegistriesMetadata(internalDevfileRegistryUrl, false)(
+      if (internalDevfileRegistryUrl) {
+        await requestRegistriesMetadata(internalDevfileRegistryUrl, false)(
+          this.store.dispatch,
+          this.store.getState,
+          undefined,
+        );
+      }
+
+      const airGapedSampleURL = new URL('/dashboard/api/airgap-sample', window.location.origin)
+        .href;
+      await requestRegistriesMetadata(airGapedSampleURL, false)(
         this.store.dispatch,
         this.store.getState,
         undefined,
