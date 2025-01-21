0c7b963494425c87307f1f6454f95fd59381010e
Oleksii Orel <oorel@redhat.com>
2025-01-22 00:23:36 +0200

    fix: code cleanup
    Signed-off-by: Oleksii Orel <oorel@redhat.com>

--- packages/dashboard-frontend/src/services/bootstrap/branding.constant.ts
+++ packages/dashboard-frontend/src/services/bootstrap/branding.constant.ts
@@ -33,7 +33,6 @@ export type BrandingDocs = {
   faq?: string;
   storageTypes: string;
   webSocketTroubleshooting: string;
-  startWorkspaceFromGit: string;
 };
 
 export type BrandingConfiguration = {
@@ -84,8 +83,6 @@ export const BRANDING_DEFAULT: BrandingData = {
       'https://www.eclipse.org/che/docs/stable/end-user-guide/url-parameter-for-the-workspace-storage/',
     webSocketTroubleshooting:
       'https://www.eclipse.org/che/docs/stable/end-user-guide/troubleshooting-network-problems/',
-    startWorkspaceFromGit:
-      'https://eclipse.dev/che/docs/stable/end-user-guide/starting-a-workspace-from-a-git-repository-url/',
   },
   configuration: {},
 };
