54ce4d1174fe100e6aacabe7b7e347f7e944cf5e
Oleksii Orel <oorel@redhat.com>
2025-01-22 15:34:17 +0200

    fix: branding data merge (#1298)
    * fix: branding data merge
    
    Signed-off-by: Oleksii Orel <oorel@redhat.com>

--- packages/dashboard-frontend/src/store/Branding/actions.ts
+++ packages/dashboard-frontend/src/store/Branding/actions.ts
@@ -12,7 +12,8 @@
 
 import common from '@eclipse-che/common';
 import { createAction } from '@reduxjs/toolkit';
-import { merge } from 'lodash';
+import cloneDeep from 'lodash/cloneDeep';
+import merge from 'lodash/merge';
 
 import { fetchApiInfo, fetchBranding } from '@/services/assets/branding';
 import { BRANDING_DEFAULT, BrandingData } from '@/services/bootstrap/branding.constant';
@@ -71,7 +72,7 @@ export const actionCreators = {
 };
 
 export function getBrandingData(receivedBranding?: { [key: string]: any }): BrandingData {
-  let branding: BrandingData = Object.assign({}, BRANDING_DEFAULT);
+  let branding: BrandingData = cloneDeep(BRANDING_DEFAULT);
 
   if (receivedBranding && Object.keys(receivedBranding).length > 0) {
     branding = merge(branding, receivedBranding);
